d6e6de65a4bb90901e29708b52a265b1bcd5dac2
Katherine Lim <k4lim@ucsd.edu>
2021-02-18 10:15:45 -0800

    button clicks go thru

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn(e) {
+	e.preventDefault();
+	ga("create", "UA-190112638-1", "auto");
+	ga("send", "event", "like", "click");
 }
